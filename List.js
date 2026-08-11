a736850a3ecf2a0cf48bc07c3323dcca21ab8100
Felisomnus <pixel19921108@gmail.com>
2026-08-12 04:35:19 +0900

    コードの整理と改良

--- List.js
+++ List.js
@@ -1,7 +1,47 @@
-function load () {
-    return (JSON.parse(localStorage.getItem("List")) || []);
+function load(key) {
+    return (JSON.parse(localStorage.getItem(key)) || []);
 }
 
-function save (List) {
-    localStorage.setItem("List", JSON.stringify(List));
+function save(data, key) {
+    localStorage.setItem(key, JSON.stringify(data));
+}
+
+function countStudents() {
+    let maleCount = 0;
+    let femaleCount = 0;
+    List.forEach(student => {
+        if (student.gender === "male") {
+            maleCount++;
+        }
+        if (student.gender === "female") {
+            femaleCount++;
+        }
+    })
+    document.querySelector(".maleCount").textContent = maleCount;
+    document.querySelector(".femaleCount").textContent = femaleCount;
+    document.querySelector(".totalCount").textContent = List.length;
+}
+
+function removeStudents() {
+    const selectedIndexes = [];
+    document.querySelectorAll("li button").forEach((element, index) => {
+        if (element.classList.contains("selected")) {
+            selectedIndexes.push(index);
+        }
+    })
+    List = List.filter((student, index) => {
+        return !selectedIndexes.includes(index);
+    })
+}
+
+function displayStudents() {
+    const students = List.map(student => {
+        const li = document.createElement("li");
+        const button = document.createElement("button");
+        button.disabled = true;
+        li.textContent = student.name;
+        li.appendChild(button);
+        return li;
+    })
+    ul.replaceChildren(...students);
 }
--- uncategorized.js
+++ uncategorized.js
@@ -1,25 +1,11 @@
-function printName() {
-    ul.innerHTML = "";
-    List.forEach(Student => {
-        const name = Student.name;
-        const li = document.createElement("li");
-        const button = document.createElement("button");
-        li.appendChild(button);
-        ul.appendChild(li);
-        li.classList.add("name");
-        button.textContent = name;
-        button.disabled = true;
-    })
-}
-
 function addName() {
     const name = text.value.replace(/[ 　]/g, "");
     const gender = document.querySelector('select[name="gender"]').value;
     if (name.length !== 0) {
         List.push(new Student(name, gender));
-        save(List);
+        save(List, "List");
     }
+    countStudents();
     text.value = "";
     text.focus();
-    count();
 }
